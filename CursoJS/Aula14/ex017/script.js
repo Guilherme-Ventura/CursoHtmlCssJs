1739575079245d649e1649f54d9ecbca02f26cd4
Guilherme-Ventura <guilherme.almeida.ventura@gmail.com>
2023-10-23 17:16:33 -0300

    criado html ex tabuada

---
+++ CursoJS/Aula14/ex017/script.js
@@ -0,0 +1,8 @@
+function gerarTabuada(){
+    let num = document.getElementById('txtnum')
+    let tabuada = document.getElementById('selectTab')
+
+    if(num.value.length == 0){
+        alert('zerado')
+    }
+}
