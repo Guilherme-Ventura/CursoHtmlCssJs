d2ffb2121030ce1f6e5a408860d941c4ae004cec
Guilherme-Ventura <guilherme.almeida.ventura@gmail.com>
2023-10-26 15:59:14 -0300

    ex tabuada

--- CursoJS/Aula14/ex017/script.js
+++ CursoJS/Aula14/ex017/script.js
@@ -1,8 +1,16 @@
 function gerarTabuada(){
     let num = document.getElementById('txtnum')
     let tabuada = document.getElementById('selectTab')
-
+    
     if(num.value.length == 0){
         alert('zerado')
+    } else{
+        let n = Number(num.value)
+        tabuada.innerHTML = ''
+        for(let i = 0; i <= 10; i++){
+            let item = document.createElement('option')
+            item.text = `${n} X ${i} = ${n * i}`
+            tabuada.appendChild(item)
+        }
     }
 }
